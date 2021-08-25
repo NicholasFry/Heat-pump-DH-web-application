725199286ea96627c34b1a82dede20ac70bd5229
Nick Fry <nicholas.a.fry@gmail.com>
2021-08-25 18:29:37 -0500

    MVP2 update

--- HeatPumpDHWeb/simulations_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include 
-from .views import *
-# from .models import *
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='base.html'), name='base'),
-    path('home/', TemplateView.as_view(template_name='home.html'), name='home')
-]
--- HeatPumpDHWeb/simulations_app/views.py
+++
@@ -1,9 +0,0 @@
-# from django.views.generic import DetailView
-
-
-
-# from .models import *
-
-# class DetailView(DetailView):
-#     model = get
-#     template_name = 'simulation.html'
